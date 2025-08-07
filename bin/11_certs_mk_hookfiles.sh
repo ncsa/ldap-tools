cbb8f180beb8e49e98b8b48874b51a55ad6c2189
Andy Loftus <aloftus@gmail.com>
2025-08-07 05:09:58 +0000

    cert deploy hooks

---
+++ bin/11_certs_mk_hookfiles.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+INSTALL_DIR='___INSTALL_DIR___'
+. "${INSTALL_DIR}"/ds_lib.sh
+
+HOOK_DIR="${LETSENCRYPT_BASE}"/renewal-hooks
+DEPLOY_DIR="${HOOK_DIR}"/deploy
+
+
+mk_deploy_hooks() {
+  pushd "${DEPLOY_DIR}"
+  for f in 12_certs_install.sh 13_certs_enable.sh ; do
+    ln -s "${INSTALL_DIR}"/$f
+  done
+  popd
+}
+
+
+###
+# Main
+###
+
+mk_deploy_hooks
