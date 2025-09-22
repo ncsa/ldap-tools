b8675660502e54f21d651bbf78a808c906b480de
Andy Loftus <aloftus@gmail.com>
2025-09-22 04:06:27 +0000

    new dump_db for v12

--- bin/dump_db.sh
+++ bin/dump_db.sh
@@ -5,7 +5,7 @@ INSTALL_DIR='___INSTALL_DIR___'
 
 
 dump_db() {
-  _dsconf backend export -r userroot
+  _dsctl db2ldif --replication userRoot
   local _ldif_out_fn=$( ls -t "${DS_LDIF_DIR}" | head -1)
   ln -s -r "${DS_LDIF_DIR}"/"${_ldif_out_fn}" "${DS_LDIF_DIR}"/replcheck_${HOST}.ldif
 }
@@ -18,6 +18,11 @@ continue_or_exit "DS389 will be stopped during the backup. Continue?"
 
 _dsctl stop
 
+ts_start=$SECONDS
 dump_db
+ts_end=$SECONDS
+elapsed=$( bc <<< "$ts_end - $ts_start" )
+echo "Database backup took: ${elapsed} secs"
 
 _dsctl start
+
